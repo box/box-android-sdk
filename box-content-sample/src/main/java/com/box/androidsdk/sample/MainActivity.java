35b1e06d8fe368392d5be8544c467884fde84c54
Ittai Shay <ishay@mbp-003078.local>
2015-07-24 15:53:48 -0700

    reverted sample app changes

--- box-content-sample/src/main/java/com/box/androidsdk/sample/MainActivity.java
+++ box-content-sample/src/main/java/com/box/androidsdk/sample/MainActivity.java
@@ -2,7 +2,6 @@ package com.box.androidsdk.sample;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
@@ -23,18 +22,14 @@ import com.box.androidsdk.content.BoxConfig;
 import com.box.androidsdk.content.BoxException;
 import com.box.androidsdk.content.BoxFutureTask;
 import com.box.androidsdk.content.auth.BoxAuthentication;
-import com.box.androidsdk.content.models.BoxArray;
 import com.box.androidsdk.content.models.BoxEntity;
 import com.box.androidsdk.content.models.BoxError;
 import com.box.androidsdk.content.models.BoxFile;
 import com.box.androidsdk.content.models.BoxFolder;
 import com.box.androidsdk.content.models.BoxItem;
 import com.box.androidsdk.content.models.BoxListItems;
-import com.box.androidsdk.content.models.BoxMetadata;
-import com.box.androidsdk.content.models.BoxMetadataUpdateTask;
 import com.box.androidsdk.content.models.BoxSession;
 import com.box.androidsdk.content.requests.BoxRequestsFile;
-import com.box.androidsdk.content.requests.BoxRequestsMetadata;
 import com.box.androidsdk.content.requests.BoxResponse;
 
 import org.apache.http.HttpStatus;
@@ -42,7 +37,6 @@ import org.apache.http.HttpStatus;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 public class MainActivity extends ActionBarActivity implements BoxAuthentication.AuthListener {
 
@@ -220,80 +214,9 @@ public class MainActivity extends ActionBarActivity implements BoxAuthentication
                 }
             }.start();
             return true;
-        } else if (id == R.id.metadata) {
-            getMetadata();
-            return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void getMetadata() {
-        mDialog = ProgressDialog.show(MainActivity.this, getText(R.string.boxsdk_Please_wait), getText(R.string.boxsdk_Please_wait));
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    String id = "33710964929";
-
-                    // Get All Metadata
-//                    BoxRequestsMetadata.GetFileMetadata request = mMetadataApi.getGetMetadataRequest(id);
-                    mMetadataApi.getGetMetadataRequest(id).send();
-
-                    // Get Template Metadata
-//                    BoxRequestsMetadata.GetFileMetadata request = mMetadataApi.getGetMetadataRequest(id, "salesContract");
-                    mMetadataApi.getGetMetadataRequest(id, "salesContract").send();
-
-                    // Get All Available Metadata Templates
-//                    BoxRequestsMetadata.GetMetadataTemplates request = mMetadataApi.getGetMetadataTemplatesRequest();
-                    mMetadataApi.getGetMetadataTemplatesRequest().send();
-
-                    // Get Schema For Specific Metadata Template
-//                    BoxRequestsMetadata.GetMetadataTemplateSchema request = mMetadataApi.getGetMetadataTemplateSchemaRequest("salesContract");
-                    mMetadataApi.getGetMetadataTemplateSchemaRequest("salesContract").send();
-
-                    // Delete Template
-                    try {
-                        mMetadataApi.getDeleteTemplateMetadataRequest(id, "invoice").send();
-                    } catch (BoxException e) {
-                        // do nothing, metadata template just doesn't exist yet for the file
-                    }
-
-                    // Make sure comment add still works
-//                    mFileApi.getAddCommentRequest(id, "Test comment 2222").send();
-
-                    // Create Template
-                    LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
-                    map.put("invoiceNumber", "12345");
-                    map.put("companyName", "Boxers");
-                    map.put("terms", "30");
-//                    BoxRequestsMetadata.AddMetadataToFile request = mMetadataApi.getAddMetadataRequest(id, map, "enterprise", "invoice");
-                    mMetadataApi.getAddMetadataRequest(id, map, "enterprise", "invoice").send();
-
-                    // Update Template
-                    BoxArray<BoxMetadataUpdateTask> updateTaskList = new BoxArray<BoxMetadataUpdateTask>();
-                    updateTaskList.add(new BoxMetadataUpdateTask(BoxMetadataUpdateTask.BoxMetadataUpdateOperations.BoxMetadataUpdateTEST, "companyName", "Boxers"));
-                    updateTaskList.add(new BoxMetadataUpdateTask(BoxMetadataUpdateTask.BoxMetadataUpdateOperations.BoxMetadataUpdateREMOVE, "companyName"));
-                    updateTaskList.add(new BoxMetadataUpdateTask(BoxMetadataUpdateTask.BoxMetadataUpdateOperations.BoxMetadataUpdateTEST, "terms", "30"));
-                    updateTaskList.add(new BoxMetadataUpdateTask(BoxMetadataUpdateTask.BoxMetadataUpdateOperations.BoxMetadataUpdateREPLACE, "terms", "60"));
-                    updateTaskList.add(new BoxMetadataUpdateTask(BoxMetadataUpdateTask.BoxMetadataUpdateOperations.BoxMetadataUpdateADD, "approved", "Yes"));
-                    BoxRequestsMetadata.UpdateFileMetadata request = mMetadataApi.getUpdateMetadataRequest(id, updateTaskList, "enterprise", "invoice");
-
-                    final BoxMetadata metadata = request.send();
-                    showToast("Metadata Request Successful!");
-                    Intent intent = new Intent(MainActivity.this, ShowMetadataActivity.class);
-                    intent.putExtra("metadata", metadata.toJson());
-                    startActivity(intent);
-                } catch (BoxException e) {
-                    e.printStackTrace();
-                    System.out.println(e.getResponse());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    mDialog.dismiss();
-                }
-            }
-        }.start();
     }
 
     private void switchAccounts() {

5be1966be43c4228d6a97b061712cccccd91b526
Brian Tang <btang@box.com>
2015-09-22 18:28:33 -0700

    Debug statements

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -3,7 +3,8 @@
 # NOTE: Travis-CI can only publish SNAPSHOT versions. To release a version, you need
 #       to use the internal Jenkins job.
 
-if [ "$TRAVIS_REPO_SLUG" == "box/box-android-sdk" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+echo Repo: $TRAVIS_REPO_SLUG Pull Request: $TRAVIS_PULL_REQUEST Branch: $TRAVIS_BRANCH
+if [ "$TRAVIS_REPO_SLUG" == "box/box-android-sdk" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
       echo 'Travis can only publish snapshots. To publish a release, use the Jenkins instance.'
       return 0

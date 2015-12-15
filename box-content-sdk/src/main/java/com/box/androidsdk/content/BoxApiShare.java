34601fc3d7ff8fd6c4f9aeebf5494d1ff9d7a5b1
seema-at-box <sarayarakavil@box.com>
2015-12-15 11:33:41 -0800

    Add method that takes a password.

--- box-content-sdk/src/main/java/com/box/androidsdk/content/BoxApiShare.java
+++ box-content-sdk/src/main/java/com/box/androidsdk/content/BoxApiShare.java
@@ -1,5 +1,7 @@
 package com.box.androidsdk.content;
 
+import android.text.TextUtils;
+
 import com.box.androidsdk.content.models.BoxSession;
 import com.box.androidsdk.content.models.BoxSharedLinkSession;
 import com.box.androidsdk.content.requests.BoxRequestsShare;
@@ -28,8 +30,22 @@ public class BoxApiShare extends BoxApi {
      * @return  request to get a BoxItem from a shared link.
      */
     public BoxRequestsShare.GetSharedLink getSharedLinkRequest(String sharedLink) {
+        return getSharedLinkRequest(sharedLink, null);
+    }
+
+    /**
+     * Returns a request to get a BoxItem from a shared link.
+     *
+     * @param sharedLink    shared link of the item to retrieve.
+     * @param password password for shared link
+     * @return  request to get a BoxItem from a shared link.
+     */
+    public BoxRequestsShare.GetSharedLink getSharedLinkRequest(String sharedLink, String password) {
         BoxSharedLinkSession session = new BoxSharedLinkSession(sharedLink, mSession);
         session.setSharedLink(sharedLink);
+        if (!TextUtils.isEmpty(password)) {
+            session.setPassword(password);
+        }
         BoxRequestsShare.GetSharedLink request = new BoxRequestsShare.GetSharedLink(getSharedItemsUrl(), session);
         return request;
     }

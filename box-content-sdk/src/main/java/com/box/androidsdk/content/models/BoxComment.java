d755b31edcfa69d984f071547233385f4487dcfd
Don Cung <dcung@box.com>
2016-02-23 14:44:50 -0800

    More refactoring

--- box-content-sdk/src/main/java/com/box/androidsdk/content/models/BoxComment.java
+++ box-content-sdk/src/main/java/com/box/androidsdk/content/models/BoxComment.java
@@ -49,10 +49,10 @@ public class BoxComment extends BoxEntity {
     /**
      * Constructs a BoxComment with the provided map values.
      *
-     * @param map map of keys and values of the object
+     * @param jsonObject represents a json object that correspends to this class.
      */
-    public BoxComment(Map<String, Object> map) {
-        super(map);
+    public BoxComment(JsonObject jsonObject) {
+        super(jsonObject);
     }
 
     /**
@@ -79,7 +79,7 @@ public class BoxComment extends BoxEntity {
      * @return info about the user who created the comment.
      */
     public BoxUser getCreatedBy() {
-        return (BoxUser) mProperties.get(FIELD_CREATED_BY);
+        return (BoxUser) getPropertyAsJsonObject(BoxEntity.getBoxJsonObjectCreator(), FIELD_CREATED_BY);
     }
 
     /**
@@ -98,7 +98,7 @@ public class BoxComment extends BoxEntity {
      * @return the item this comment is attached to.
      */
     public BoxItem getItem() {
-        return (BoxItem) mProperties.get(FIELD_ITEM);
+        return (BoxItem) getPropertyAsJsonObject(BoxEntity.getBoxJsonObjectCreator(), FIELD_ITEM);
     }
 
     /**
@@ -110,54 +110,4 @@ public class BoxComment extends BoxEntity {
         return getPropertyAsDate(FIELD_MODIFIED_AT);
     }
 
-    @Override
-    protected void parseJSONMember(JsonObject.Member member) {
-        try {
-            String memberName = member.getName();
-            JsonValue value = member.getValue();
-            if (memberName.equals(FIELD_IS_REPLY_COMMENT)) {
-                this.mProperties.put(FIELD_IS_REPLY_COMMENT, value.asBoolean());
-                return;
-            } else if (memberName.equals(FIELD_MESSAGE)) {
-                this.mProperties.put(FIELD_MESSAGE, value.asString());
-                return;
-            } else if (memberName.equals(FIELD_TAGGED_MESSAGE)) {
-                this.mProperties.put(FIELD_TAGGED_MESSAGE, value.asString());
-                return;
-            } else if (memberName.equals(FIELD_CREATED_BY)) {
-                BoxUser createdBy = new BoxUser();
-                createdBy.createFromJson(value.asObject());
-                this.mProperties.put(FIELD_CREATED_BY, createdBy);
-                return;
-            } else if (memberName.equals(FIELD_CREATED_AT)) {
-                this.mProperties.put(FIELD_CREATED_AT, BoxDateFormat.parse(value.asString()));
-                return;
-            } else if (memberName.equals(FIELD_MODIFIED_AT)) {
-                this.mProperties.put(FIELD_MODIFIED_AT, BoxDateFormat.parse(value.asString()));
-                return;
-            } else if (memberName.equals(FIELD_ITEM)) {
-                JsonObject itemObj = value.asObject();
-                String itemType = itemObj.get(BoxEntity.FIELD_TYPE).asString();
-                BoxEntity entity;
-                if (itemType.equals(BoxFile.TYPE)) {
-                    entity = new BoxFile();
-                    entity.createFromJson(itemObj);
-                } else if (itemType.equals(BoxComment.TYPE)) {
-                    entity = new BoxComment();
-                    entity.createFromJson(itemObj);
-                } else if (itemType.equals(BoxBookmark.TYPE)) {
-                    entity = new BoxBookmark();
-                    entity.createFromJson(itemObj);
-                } else {
-                    throw new IllegalArgumentException(String.format(Locale.ENGLISH, "Unsupported type \"%s\" for comment found", itemType));
-                }
-                this.mProperties.put(FIELD_ITEM, entity);
-                return;
-            }
-        } catch (ParseException e) {
-            assert false : "A ParseException indicates a bug in the SDK.";
-        }
-
-        super.parseJSONMember(member);
-    }
 }

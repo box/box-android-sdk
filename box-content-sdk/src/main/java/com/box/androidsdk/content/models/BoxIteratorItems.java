5a4b85a7db151884eae97c695f2267344c9e9f44
Varun Gupta <varungupta@ad.whirl.net>
2016-02-25 15:23:37 -0800

    Add jsonObject constructor in BoxIteratorItems

--- box-content-sdk/src/main/java/com/box/androidsdk/content/models/BoxIteratorItems.java
+++ box-content-sdk/src/main/java/com/box/androidsdk/content/models/BoxIteratorItems.java
@@ -1,5 +1,9 @@
 package com.box.androidsdk.content.models;
 
+import com.eclipsesource.json.JsonObject;
+
+import org.json.JSONArray;
+
 /**
  * Class representing a list of items in Box (of types {@link BoxFolder}, {@link BoxFile}, and {@link BoxBookmark}).
  */
@@ -7,5 +11,9 @@ public class BoxIteratorItems extends BoxIteratorBoxEntity<BoxItem> {
     private static final long serialVersionUID = 1378358978076482578L;
 
     public BoxIteratorItems() {super();}
+
+    public BoxIteratorItems(JsonObject object) {
+        super(object);
+    }
 
 }

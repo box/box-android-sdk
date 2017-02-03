251d93f45709bf8d8022a33a779dad7c3b61e4b7
Nyjal Augustine <nyjal@box.com>
2017-02-03 15:01:26 -0800

    AND-7057: Store the GET response in levelDB and populate the UI
    Creating necessary constuctors for BoxIteratorRecentItems

--- box-content-sdk/src/main/java/com/box/androidsdk/content/models/BoxIteratorRecentItems.java
+++ box-content-sdk/src/main/java/com/box/androidsdk/content/models/BoxIteratorRecentItems.java
@@ -1,5 +1,7 @@
 package com.box.androidsdk.content.models;
 
+import com.eclipsesource.json.JsonObject;
+
 /**
  * A class representing list of BoxRecentItem's
  */
@@ -7,6 +9,14 @@ public class BoxIteratorRecentItems extends BoxIterator<BoxRecentItem> {
     private static final long serialVersionUID = -2642748896882484555L;
 
     private transient BoxJsonObjectCreator<BoxRecentItem> representationCreator;
+
+    public BoxIteratorRecentItems() {
+        super();
+    }
+
+    public BoxIteratorRecentItems(JsonObject jsonObject) {
+        super(jsonObject);
+    }
 
     @Override
     protected BoxJsonObjectCreator<BoxRecentItem> getObjectCreator() {

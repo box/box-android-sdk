c4be212d0d27f95fb3e6576bab4aadaf7691471d
Seema Arayarakavil <sarayarakavil@box.com>
2018-01-19 18:27:59 -0800

    AND 8086 Tests for common file requests

--- box-content-sdk/src/test/java/com/box/androidsdk/content/requests/BoxDownloadRequestTest.java
+++ box-content-sdk/src/test/java/com/box/androidsdk/content/requests/BoxDownloadRequestTest.java
@@ -113,4 +113,55 @@ public class BoxDownloadRequestTest extends PowerMock {
         Assert.assertEquals(version, downloadFileRequest.getVersion());
 
     }
+
+
+    @Test
+    public void testDownloadThumbnailRequest()  throws Exception {
+        BoxApiFile fileApi = new BoxApiFile(SessionUtil.newMockBoxSession(mMockContext));
+
+        //Byte stream to capture the outputstream to the server
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(1024);
+        final Date now = new Date(System.currentTimeMillis());
+        final int contentLength = DOWNLOAD_CONTENT.getBytes().length;
+
+        //Mock httpurlconnection for downloads
+        URL u = PowerMockito.mock(URL.class);
+        PowerMockito.whenNew(URL.class).withAnyArguments().thenReturn(u);
+        HttpURLConnection huc = PowerMockito.mock(HttpURLConnection.class);
+        PowerMockito.when(u.openConnection()).thenReturn(huc);
+        PowerMockito.when(huc.getOutputStream()).thenReturn(outputStream);
+        PowerMockito.when(huc.getContentType()).thenReturn(BoxRequest.ContentTypes.JSON.toString());
+        PowerMockito.when(huc.getResponseCode()).thenReturn(200);
+        PowerMockito.when(huc.getHeaderField("Content-Length")).thenReturn(String.valueOf(contentLength));
+
+        //Mock Response to use sample json
+        BoxHttpResponse response = new BoxHttpResponse(huc);
+        PowerMockito.when(response.getContentLength()).thenReturn(contentLength);
+        PowerMockito.when(response.getBody()).thenReturn(new ByteArrayInputStream(DOWNLOAD_CONTENT.getBytes()));
+        PowerMockito.whenNew(BoxHttpResponse.class).withAnyArguments().thenReturn(response);
+
+
+        OutputStream outputStream1 = new ByteArrayOutputStream(contentLength);
+        BoxRequestsFile.DownloadThumbnail downloadThumbnailRequest = fileApi.getDownloadThumbnailRequest(outputStream1, FILE_ID);
+
+        Assert.assertEquals(FILE_ID, downloadThumbnailRequest.getId());
+        downloadThumbnailRequest.setProgressListener(new ProgressListener() {
+            @Override
+            public void onProgressChanged(long numBytes, long totalBytes) {
+                mBytesRead += numBytes;
+            }
+        });
+
+        downloadThumbnailRequest.setDownloadStartListener(new DownloadStartListener() {
+            @Override
+            public void onStart(BoxDownload downloadInfo) {
+                Assert.assertNotNull(downloadInfo);
+                Assert.assertEquals(contentLength, downloadInfo.getContentLength().longValue());
+            }
+        });
+
+        //Test send
+        downloadThumbnailRequest.send();
+        Assert.assertEquals(DOWNLOAD_CONTENT.getBytes().length, mBytesRead);
+    }
 }

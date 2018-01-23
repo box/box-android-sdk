86ce50dd793b6b6cedd18e2e17c8eca2608995ef
Seema Arayarakavil <sarayarakavil@box.com>
2018-01-22 18:32:30 -0800

    AND 8088, AND 8089 Search and User Requests

--- box-content-sdk/src/test/java/com/box/androidsdk/content/requests/BoxUserRequestTest.java
+++ box-content-sdk/src/test/java/com/box/androidsdk/content/requests/BoxUserRequestTest.java
@@ -1,16 +1,31 @@
 package com.box.androidsdk.content.requests;
 
+import android.content.Context;
+
 import com.box.androidsdk.content.BoxApiUser;
+import com.box.androidsdk.content.models.BoxIteratorUsers;
 import com.box.androidsdk.content.models.BoxUser;
+import com.box.androidsdk.content.testUtil.PowerMock;
+import com.box.androidsdk.content.testUtil.SessionUtil;
+import com.eclipsesource.json.JsonObject;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 
-public class BoxUserRequestTest {
+@PrepareForTest({ BoxHttpResponse.class, BoxHttpRequest.class, BoxRequest.class, BoxRequestsUser.class})
+public class BoxUserRequestTest extends PowerMock {
+
+    private static final String USER_ID = "10543463";
+    private static final String SAMPLE_USER_JSON = "{ \"type\": \"user\", \"id\": \"10543463\", \"name\": \"Arielle Frey\", \"login\": \"ariellefrey@box.com\", \"created_at\": \"2011-01-07T12:37:09-08:00\", \"modified_at\": \"2014-05-30T10:39:47-07:00\", \"language\": \"en\", \"timezone\": \"America/Los_Angeles\", \"space_amount\": 10737418240, \"space_used\": 558732, \"max_upload_size\": 5368709120, \"status\": \"active\", \"job_title\": \"\", \"phone\": \"\", \"address\": \"\", \"avatar_url\": \"https://app.box.com/api/avatar/deprecated\" }";
+
+    @Mock
+    Context mMockContext;
 
     @Test
     public void testCreateEnterpriseUserRequestProperties() throws ParseException {
@@ -83,4 +98,66 @@ public class BoxUserRequestTest {
         String json = request.getStringBody();
         Assert.assertEquals(expected, json);
     }
+
+    @Test
+    public void testUserInfoRequest() throws Exception {
+        final String expectedRequestUrl = "https://api.box.com/2.0/users/" + USER_ID;
+
+        BoxApiUser userApi = new BoxApiUser(SessionUtil.newMockBoxSession(mMockContext));
+        BoxRequestsUser.GetUserInfo userInfoRequest = userApi.getUserInfoRequest(USER_ID);
+
+        mockSuccessResponseWithJson(SAMPLE_USER_JSON);
+        BoxUser boxUser = userInfoRequest.send();
+
+        Assert.assertEquals(expectedRequestUrl, userInfoRequest.mRequestUrlString);
+        Assert.assertEquals(JsonObject.readFrom(SAMPLE_USER_JSON), boxUser.toJsonObject());
+
+    }
+
+    @Test
+    public void testCurrentUserInfoRequest() throws Exception {
+        final String expectedRequestUrl = "https://api.box.com/2.0/users/me";
+
+        BoxApiUser userApi = new BoxApiUser(SessionUtil.newMockBoxSession(mMockContext));
+        BoxRequestsUser.GetUserInfo userInfoRequest = userApi.getCurrentUserInfoRequest();
+
+        mockSuccessResponseWithJson(SAMPLE_USER_JSON);
+        BoxUser boxUser = userInfoRequest.send();
+
+        Assert.assertEquals(expectedRequestUrl, userInfoRequest.mRequestUrlString);
+        Assert.assertEquals(JsonObject.readFrom(SAMPLE_USER_JSON), boxUser.toJsonObject());
+
+    }
+
+    @Test
+    public void testMockCreateEnterpriseUserRequest() throws Exception {
+        final String expectedRequestUrl = "https://api.box.com/2.0/users";
+
+        BoxApiUser userApi = new BoxApiUser(SessionUtil.newMockBoxSession(mMockContext));
+        BoxRequestsUser.CreateEnterpriseUser userInfoRequest = userApi.getCreateEnterpriseUserRequest("ariellefrey@box.com", "Arielle Frey");
+
+        mockSuccessResponseWithJson(SAMPLE_USER_JSON);
+        BoxUser boxUser = userInfoRequest.send();
+
+        Assert.assertEquals(expectedRequestUrl, userInfoRequest.mRequestUrlString);
+        Assert.assertEquals(JsonObject.readFrom(SAMPLE_USER_JSON), boxUser.toJsonObject());
+
+    }
+
+    @Test
+    public void testGetEnterpriseUsersRequest() throws Exception {
+        final String expectedRequestUrl = "https://api.box.com/2.0/users";
+        final String enterpriseUsersJson = "{ \"total_count\": 1, \"entries\": [ { \"type\": \"user\", \"id\": \"181216415\", \"name\": \"sean rose\", \"login\": \"sean+awesome@box.com\", \"created_at\": \"2012-05-03T21:39:11-07:00\", \"modified_at\": \"2012-08-23T14:57:48-07:00\", \"language\": \"en\", \"space_amount\": 5368709120, \"space_used\": 52947, \"max_upload_size\": 104857600, \"status\": \"active\", \"job_title\": \"\", \"phone\": \"5555551374\", \"address\": \"10 Cloud Way Los Altos CA\", \"avatar_url\": \"https://app.box.com/api/avatar/large/181216415\" } ] }";
+        BoxApiUser userApi = new BoxApiUser(SessionUtil.newMockBoxSession(mMockContext));
+        BoxRequestsUser.GetEnterpriseUsers enterpriseUsersRequest = userApi.getEnterpriseUsersRequest();
+
+        mockSuccessResponseWithJson(enterpriseUsersJson);
+        BoxIteratorUsers boxUsers = enterpriseUsersRequest.send();
+
+        Assert.assertEquals(expectedRequestUrl, enterpriseUsersRequest.mRequestUrlString);
+        Assert.assertEquals(JsonObject.readFrom(enterpriseUsersJson), boxUsers.toJsonObject());
+
+    }
+
+
 }

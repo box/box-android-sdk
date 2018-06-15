aee3b891f490bf6b56bbb94cc358f9291303d612
Don Cung <dcung@box.com>
2018-06-15 15:16:00 -0700

    Change version to 4.2.0

--- box-content-sdk/src/main/java/com/box/androidsdk/content/BoxConfig.java
+++ box-content-sdk/src/main/java/com/box/androidsdk/content/BoxConfig.java
@@ -75,7 +75,7 @@ public class BoxConfig {
     /**
      * Version string
      */
-    public static String SDK_VERSION = "4.1.0";
+    public static String SDK_VERSION = "4.2.0";
 
     /**
      * Optional paramater for whether authentication should use

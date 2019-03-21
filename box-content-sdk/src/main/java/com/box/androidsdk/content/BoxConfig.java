aa0093c8521e8767cfb444e7e35a860781258fa4
Sudeep Bhowmick <sbhowmick@box.com>
2019-03-21 16:58:14 -0700

    Upgrade the version to 4.2.4

--- box-content-sdk/src/main/java/com/box/androidsdk/content/BoxConfig.java
+++ box-content-sdk/src/main/java/com/box/androidsdk/content/BoxConfig.java
@@ -75,7 +75,7 @@ public class BoxConfig {
     /**
      * Version string
      */
-    public static String SDK_VERSION = "4.2.3";
+    public static String SDK_VERSION = "4.2.4";
 
     /**
      * Optional paramater for whether authentication should use

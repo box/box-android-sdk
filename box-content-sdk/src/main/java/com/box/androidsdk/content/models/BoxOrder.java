35ca786b86f298332635f24a280b56d7d9d42332
dcung <dcung@box.com>
2020-01-16 13:28:50 -0800

    Adds support for sorting and marker based pagination.

--- box-content-sdk/src/main/java/com/box/androidsdk/content/models/BoxOrder.java
+++ box-content-sdk/src/main/java/com/box/androidsdk/content/models/BoxOrder.java
@@ -11,6 +11,15 @@ public class BoxOrder extends BoxJsonObject {
     public static final String FIELD_BY = "by";
     public static final String FIELD_DIRECTION = "direction";
 
+    public static final String DIRECTION_ASCENDING = "ASC";
+    public static final String DIRECTION_DESCENDING = "DESC";
+
+    public static final String SORT_ID = "id";
+    public static final String SORT_NAME = "name";
+    public static final String SORT_DATE = "date";
+    public static final String SORT_SIZE = "size";
+
+
     public String getBy() {
         return getPropertyAsString(FIELD_BY);
     }
